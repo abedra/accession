6736424e3286529d6501091e51662f78911eaf60
Aaron Bedra <aaron@aaronbedra.com>
2011-11-02 19:58:05 -0400

    Macro time

--- src/accession/core.clj
+++ src/accession/core.clj
@@ -1,10 +1,14 @@
 (ns accession.core
+  (:refer-clojure :exclude [get set])
   (:require [clojure.string :as str]
             [clojure.java.io :as io])
   (:import (java.net Socket)
            (java.io InputStreamReader)))
 
 (defmulti response (fn [r] (char (.read r))))
+
+(defmethod response \- [rdr]
+  (.readLine rdr))
 
 (defmethod response \+ [rdr]
   (.readLine rdr))
@@ -21,7 +25,7 @@
         resp (repeatedly (* 2 length) (fn [] (conj [] (.readLine rdr))))]
     (mapcat second (partition 2 resp))))
 
-(defn command [name & args]
+(defn query [name & args]
   (str "*"
        (+ 1 (count args)) "\r\n"
        "$" (count name) "\r\n"
@@ -33,7 +37,7 @@
                   args))
        "\r\n"))
 
-(defn send-command [cmd]
+(defn request [cmd]
   (let [socket (doto (Socket. "127.0.0.1" 6379)
                  (.setTcpNoDelay true)
                  (.setKeepAlive true))
@@ -42,3 +46,38 @@
         rdr (io/reader (InputStreamReader. in))]
     (.write out (.getBytes cmd))
     (response rdr)))
+
+(defmacro defquery
+  [name params]
+  (let [command (str name)]
+    `(defn ~name ~params
+       (request (query ~command ~@params)))))
+
+(defmacro defqueries
+  [& queries]
+  `(do ~@(map (fn [q] `(defquery ~@q)) queries)))
+
+(defqueries
+  (set         [key value])
+  (get         [key])
+
+  (incr        [key])
+
+  (del         [key])
+  (expire      [key seconds])
+  (ttl         [key])
+
+  (rpush       [key value])
+  (lpush       [key value])
+  (llen        [key])
+  (lrange      [key start end])
+  (lpop        [key])
+  (rpop        [key])
+
+  (sadd        [key member])
+  (srem        [key member])
+  (sismember   [key member])
+  (sunion      [set1 set2])
+
+  (zadd        [key score member])
+  (zrange      [key start end]))
