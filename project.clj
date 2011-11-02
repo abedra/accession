457af722d98b4b2d03f7a7a3f870460c61514dc0
Aaron Bedra <aaron@aaronbedra.com>
2011-11-02 13:08:19 -0400

    Use Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject accession "0.0.1-SNAPSHOT"
   :description "A Clojure library for Redis"
-  :dependencies [[org.clojure/clojure "1.4.0-alpha1"]])
+  :dependencies [[org.clojure/clojure "1.4.0-alpha1"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]])
