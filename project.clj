527e76c05773f8258939e2c8cd403f023d284106
fogus <mefogus@gmail.com>
2011-11-29 12:26:58 -0500

    Rolled Marg back to a more stable version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject accession "0.1.0"
   :description "A Clojure library for Redis"
   :dependencies [[org.clojure/clojure "1.4.0-alpha1"]]
-  :dev-dependencies [[marginalia "0.7.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+  :dev-dependencies [[marginalia "0.6.1" :exclusions [org.clojure/clojure]]
                      [lein-marginalia "0.6.1" :exclusions [org.clojure/clojure]]
                      [lein-clojars "0.6.0" :exclusions [org.clojure/clojure]]])
