6412215fa702399e7eed0f6b1314a6a95b22dc70
Aaron Bedra <aaron@aaronbedra.com>
2012-10-13 17:02:53 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject accession "0.1.1"
+(defproject accession "0.1.2"
   :description "A Clojure library for Redis"
-  :dependencies [[org.clojure/clojure "1.4.0-alpha2"]]
-  :dev-dependencies [[marginalia "0.6.1" :exclusions [org.clojure/clojure]]
-                     [lein-marginalia "0.6.1" :exclusions [org.clojure/clojure]]
-                     [lein-clojars "0.6.0" :exclusions [org.clojure/clojure]]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[marginalia "0.7.1" :exclusions [org.clojure/clojure]]
+                     [lein-marginalia "0.7.1" :exclusions [org.clojure/clojure]]
+                     [lein-clojars "0.9.1" :exclusions [org.clojure/clojure]]])
