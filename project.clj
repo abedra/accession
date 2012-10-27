28e9ec718dee825ddd0aeb7e46485b6867410539
Aaron Bedra <aaron@aaronbedra.com>
2012-10-27 12:15:34 -0500

    Making the move to lein 2
    Signed-off-by: Aaron Bedra <aaron@aaronbedra.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
-(defproject accession "0.1.2"
+(defproject accession "0.2.0-snapshot"
   :description "A Clojure library for Redis"
+  :url "https://github.com/abedra/accession"
+  :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[marginalia "0.7.1" :exclusions [org.clojure/clojure]]
-                     [lein-marginalia "0.7.1" :exclusions [org.clojure/clojure]]
-                     [lein-clojars "0.9.1" :exclusions [org.clojure/clojure]]])
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-beta1"]]}
+             :dev {:dependencies [[marginalia "0.7.1" :exclusions [org.clojure/clojure]]
+                                  [lein-marginalia "0.7.1" :exclusions [org.clojure/clojure]]
+                                  [lein-clojars "0.9.1" :exclusions [org.clojure/clojure]]]}}
+  :aliases {"multi-test" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
+  :min-lein-version "2.0.0"
+  :warn-on-reflection false)
